e77ed5dd5e8af6011e4a97bc08442d7bd539767f
eslamvet1 <58374613+eslamvet@users.noreply.github.com>
2024-01-14 18:53:31 +0200

    deploy to github pages

--- src/app/guards/authentication.guard.ts
+++ src/app/guards/authentication.guard.ts
@@ -1,9 +1,10 @@
 import { UserService } from '@services/user.service';
 import { CanMatchFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
+import { BASE_HREF } from '@utils/base-href';
 
 export const authenticationGuard: CanMatchFn = (route, segments) => {
   const userService = inject(UserService)
   const router = inject(Router)
-  return userService.getUser() ? true : router.parseUrl('/')
+  return userService.getUser() ? true : router.parseUrl(BASE_HREF)
 };
